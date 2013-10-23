b9cf1550d15246af82eb2ded49cd43bed429b7bb
Hector Rios <oj.rios@gmail.com>
2013-10-23 22:38:24 +0200

    Add a TableViewController to the storyboard and create a subclass of the UITableViewController to be the controller for our view

---
+++ UITableViewController Challenge Solution/CSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Hector Rios on 10/23/13.
+//  Copyright (c) 2013 Hector Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSTableViewController : UITableViewController
+
+@end
